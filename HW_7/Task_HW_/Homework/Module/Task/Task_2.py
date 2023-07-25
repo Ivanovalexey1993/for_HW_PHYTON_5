bb3b6a50aa4955ba80b3be940b6548dd50831475
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2023-07-25 10:05:29 +0300

    HW 7-2

---
+++ HW_7/Task_HW_/Homework/Module/Task/Task_2.py
@@ -0,0 +1,3 @@
+from Seminar_7.Task_HW.Homework.Module.Module_Task_2 import random_name
+
+random_name(20)
---
+++ HW_7/Task_HW_/Homework/Module/Task/Task_3.py
@@ -0,0 +1,4 @@
+from Seminar_7.Task_HW.Homework.Module.Module_Task_3 import whatever
+
+
+whatever()
